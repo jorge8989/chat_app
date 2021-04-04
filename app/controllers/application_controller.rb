92e257670f8880c9a7e724850b39876098735642
jorge8989 <jorgearteaga8936@gmail.com>
2021-04-03 22:51:54 -0400

    verify user when attempting ActionCable connection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
+  include ActionController::Cookies
+
   before_action :authenticate_user!
+  before_action :set_user_cookie
+
+  private
+  def set_user_cookie
+    if !cookies&.signed[:user_id] && current_user
+      cookies.signed[:user_id] = current_user.id
+    end
+  end
 end
